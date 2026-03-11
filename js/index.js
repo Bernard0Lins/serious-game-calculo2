7ddecee7de81daf35d325ca572a3480d028a6614
Bernard0lins <linsbentesbernardo@gmail.com>
2026-03-11 20:01:40 -0300

    feat : criação da pagina inicial do jogo

---
+++ js/index.js
@@ -0,0 +1,24 @@
+// js/index.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const btnIniciar = document.getElementById("btn-iniciar");
+
+    if (btnIniciar) {
+        btnIniciar.addEventListener("click", () => {
+            
+            const btnText = btnIniciar.querySelector('.btn-text');
+            const btnIcon = btnIniciar.querySelector('.btn-icon');
+            
+            if (btnText) btnText.innerText = "Acessando fechadura... aguarde...";
+            if (btnIcon) btnIcon.innerText = "🔄"; 
+            
+            // Novas cores de feedback do botão (Branco e Azul Marinho)
+            btnIniciar.style.backgroundColor = "#ffffff";
+            btnIniciar.style.color = "#0A142F";
+            
+            setTimeout(() => {
+                window.location.href = "pages/sala1.html"; 
+            }, 1500);
+        });
+    }
+});
